3180be3a12455b78e141add6d7d8687d98460c26
LaveeshkaMahadea <laveeshka94@gmail.com>
2022-12-08 20:24:21 +1100

    go back to Workout page from Exercises page

--- src/pages/ExistingExercises/ExistingExercises.jsx
+++ src/pages/ExistingExercises/ExistingExercises.jsx
@@ -1,12 +1,21 @@
 import { useContext } from 'react';
 import { ExercisesContext } from "./../../context/exercises";
+import { useNavigate } from "react-router-dom";
 import Box from "@mui/material/Box";
+import CommonIconButton from '../../components/common/IconButton/IconButton';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 
 
 function ExistingExercises(){
     const {exercises, setExercises, isExercisesLoaded} = useContext(ExercisesContext);
+    const navigate = useNavigate();
+
     if (isExercisesLoaded){
         console.log("exercises are ", exercises);
+    }
+
+    function handleClickBackButton(){
+        navigate("/workout");
     }
 
     return (
@@ -17,7 +26,9 @@ function ExistingExercises(){
             paddingTop: 1,
             position: "relative",
           }}>
-
+            <CommonIconButton color="heading" size="small" handleClick={handleClickBackButton}>
+                <ArrowBackIosIcon/>
+            </CommonIconButton>
         </Box>
     )
 }
